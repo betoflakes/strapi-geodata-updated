fa35955296bcca85a31f67a8a626d6189fea26aa
JR Saucedo <jr@devsigners.mx>
2026-09-23 20:40:51 -0600

    fix: update tile attribution in Input component for better compliance with OSM guidelines

--- admin/src/components/Input.tsx
+++ admin/src/components/Input.tsx
@@ -39,7 +39,7 @@ const mapProps = {
   zoom: 15,
   center: [41.9, 12.5] as LatLngTuple,
   tileUrl: `https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`,
-  tileAttribution: 'OSM attribution',
+  tileAttribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 };
 
 const Input: React.FC<InputProps> = ({ hint, labelAction, label, name, required, ...props }) => {
